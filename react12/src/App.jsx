2c03f21d015213e081defe781ed19ead8e1512e2
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2025-04-30 12:49:47 +0530

    tST

--- react12/src/App.jsx
+++ react12/src/App.jsx
@@ -1,13 +1,11 @@
-import Message from "./Message/Message";
-//import './assets/bs/bootstrap.css'
+import CompA from './props/CompA';
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
 let App = ()=>{
 
 return <div>
             <h1>App Componet</h1>
-            <button className="btn btn-danger">Test</button>
-            <hr />
-            <Message/>
+            <hr/>
+            <CompA/>
            
        </div>
 
