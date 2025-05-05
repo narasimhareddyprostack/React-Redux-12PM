1f68e51192cba7723f2dc3ae8ff104646ae4c870
Narasimha Reddy <49887403+narasimhareddyprostack@users.noreply.github.com>
2025-05-05 13:17:36 +0530

    t

--- react12/src/App.jsx
+++ react12/src/App.jsx
@@ -1,11 +1,13 @@
 import User from "./Props2/User";
 import Message from "./stateEx1/Message";
+import Counter from "./stateEx1/Counter";
+import Product from './stateEx1/Product'
 let App = ()=>{
 
 return <div>
             <h1>App Componet</h1>
             <hr/>
-            <Message/>
+            <Product/>
            
        </div>
 
