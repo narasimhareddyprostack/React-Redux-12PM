9a3f3a057ac8b1e7ced323b99559a6c4acc99bf6
Narasimha Reddy <49887403+narasimhareddyprostack@users.noreply.github.com>
2025-05-06 13:08:53 +0530

    ts

--- react12/src/App.jsx
+++ react12/src/App.jsx
@@ -1,17 +1,14 @@
-import User from "./Props2/User";
-import Message from "./stateEx1/Message";
-import Counter from "./stateEx1/Counter";
-import Product from './stateEx1/Product'
+import Message from "./EventBinding/Message";
+import Employee from './EventBinding/Employee'
+import CompA from "./PD/CompA";
 let App = ()=>{
 
 return <div>
             <h1>App Componet</h1>
             <hr/>
-            <Product/>
+            <CompA/>
            
        </div>
 
 }
-
-
 export default App;
