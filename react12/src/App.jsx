50bc0f4fb47375c1716e36e4a3ecf3377c06e8e9
Narasimha Reddy <49887403+narasimhareddyprostack@users.noreply.github.com>
2025-05-08 13:40:35 +0530

    TS

--- react12/src/App.jsx
+++ react12/src/App.jsx
@@ -1,13 +1,15 @@
 //import Message from "./EventBinding/Message";
 //import Employee from './EventBinding/Employee'
-import Employee from "./ListRendering/Employee";
+/* import Employee from "./ListRendering/Employee";
 import CompA from "./PD/CompA";
+ */
+import Users from "./HTTP/Users";
 let App = ()=>{
 
 return <div>
             <h1>App Componet</h1>
             <hr/>
-            <Employee/>
+            <Users/>
            
        </div>
 
